58672d43ba9fa60e68998fea63f91b4ba268fb15
Tatenda Ephraim Chipwanya <tatendachipwanya@outlook.com>
2021-08-14 23:52:45 +0200

    adding About page to site

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { graphql, Link } from 'gatsby';
+
+export default ({data}) => (
+  <React.Fragment>
+    <h2>{data.site.siteMetadata.title}</h2>
+    <div>{data.site.siteMetadata.description}</div>
+    <Link to="/">Back to home</Link>
+  </React.Fragment>
+)
+export const query = graphql `
+query MyQuery {
+  site{
+    siteMetadata {
+      author, 
+      description, 
+      title
+    }
+  }
+}
+`
